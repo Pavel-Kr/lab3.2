806c4a24bf0d5b4959ce3974cc79f583cd76dad1
IronDust <pkrivosheev@bk.ru>
2021-10-26 20:56:04 +0700

    change js

--- main.js
+++ main.js
@@ -1,29 +1,39 @@
+function setCookie(name, value, expire) {
+	document.cookie = name + "=" + escape(value) +
+    ((expire == null) ? "" : ("; expires=" + expire.toGMTString()))
+}
+
+function getCookie(Name) {
+	var search = Name + "="
+	if (document.cookie.length > 0) {
+		offset = document.cookie.indexOf(search)
+		if (offset != -1) {
+			offset += search.length
+			end = document.cookie.indexOf(";", offset)
+			if (end == -1)
+				end = document.cookie.length
+			return unescape(document.cookie.substring(offset, end))
+		}
+	}
+}
 function Login(){
   let name=document.getElementById("form").name.value;
   let today=new Date();
   let expire=new Date();
   expire.setTime(today.getTime()+1000*3600);
   if(name!="")
-    document.cookie="name="+escape(name)+"; expires="+expire.toGMTString();
+    setCookie("name",name,expire);
   let hello=document.getElementById("hello");
-  hello.innerHTML="Привет "+name;
+  hello.innerHTML="Привет "+getCookie("name");
   alert(document.cookie);
 }
 function print(){
   alert(document.cookie);
 }
-window.onload=function hello(){
-  let search="name=";
-  let name;
-  if(document.cookie.length>0){
-    let offset=document.cookie.indexOf(search);
-    if(offset!=-1){
-      offset+=search.length;
-      end=document.cookie.indexOf(";",offset);
-      if(end==-1) end=document.cookie.length;
-      else name=unescape(document.cookie.substring(offset,end));
-    }
-  }
+function hello(){
+  let name=getCookie("name");
+  console.log(name);
   let hello=document.getElementById("hello");
   hello.innerHTML="Привет "+name;
 }
+window.onload=hello();
