60c1d2f04fdb065a34b5c77765edf6073fa19d22
IronDust <pkrivosheev@bk.ru>
2021-10-27 19:09:37 +0700

    mai vork is dan

--- main.js
+++ main.js
@@ -21,19 +21,52 @@ function Login(){
   let today=new Date();
   let expire=new Date();
   expire.setTime(today.getTime()+1000*3600);
-  if(name!="")
+  if(name!=""){
     setCookie("name",name,expire);
+    setCookie("visits",1,expire);
+  }
   let hello=document.getElementById("hello");
   hello.innerHTML="Привет "+getCookie("name");
-  alert(document.cookie);
+  let visit=document.getElementById("visits");
+  visit.innerHTML="Количество посещений: "+getCookie("visits");
+  document.getElementById("form").name.value=" ";
 }
-function print(){
-  alert(document.cookie);
+function Obnul(){
+  let today=new Date();
+  let expire=new Date();
+  expire.setTime(today.getTime()+1000*3600);
+  setCookie("visits",0,expire);
+  let visit=document.getElementById("visits");
+  visit.innerHTML="Количество посещений: "+getCookie("visits");
 }
-function hello(){
+function Hello(){
   let name=getCookie("name");
-  console.log(name);
+  let visits=getCookie("visits");
+  visits++;
   let hello=document.getElementById("hello");
+  let visit=document.getElementById("visits");
   hello.innerHTML="Привет "+name;
+  visit.innerHTML="Количество посещений: "+visits;
+  let today=new Date();
+  let expire=new Date();
+  expire.setTime(today.getTime()+1000*3600);
+  setCookie("visits",visits,expire);
+  let lastVisit=new Date(getCookie("time"));
+  console.log(lastVisit);
+  let hours=today.getHours()-lastVisit.getHours();
+  let minutes=today.getMinutes()-lastVisit.getMinutes();
+  if(minutes<0){
+    hours--;
+    minutes+=60;
+  }
+  let seconds=today.getSeconds()-lastVisit.getSeconds();
+  if(seconds<0){
+    minutes--;
+    seconds+=60;
+  }
+  let time=document.getElementById("time");
+  time.innerHTML="Последний раз вы были на странице " + hours + " часов, " + minutes
+      + " минут, "+seconds+" секунд назад";
+  setCookie("time",today,expire);
 }
-//window.onload=hello();
+window.onload=Hello();
